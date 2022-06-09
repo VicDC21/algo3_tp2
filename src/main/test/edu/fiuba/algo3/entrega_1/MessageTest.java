9c71afae06a1d0acca55a846742d3786f8c06a79
DiegoS <disanchez@fi.uba.ar>
2022-06-08 22:09:52 -0300

    Paquetes para entregas (#15)

--- src/main/test/edu/fiuba/algo3/entrega_1/MessageTest.java
+++ src/main/test/edu/fiuba/algo3/entrega_1/MessageTest.java
@@ -1,5 +1,6 @@
-package edu.fiuba.algo3.modelo;
+package edu.fiuba.algo3.entrega_1;
 
+import edu.fiuba.algo3.modelo.Message;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
