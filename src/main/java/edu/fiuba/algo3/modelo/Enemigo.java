26792545b328e38ed3703555e3b822b32a65f926
ignaciodestefanis <ignaciojoeldes@gmail.com>
2023-06-01 15:54:55 -0300

    Modifica Enemigo para que no pueda recibir daño si ya está muerto

--- src/main/java/edu/fiuba/algo3/modelo/Enemigo.java
+++ src/main/java/edu/fiuba/algo3/modelo/Enemigo.java
@@ -23,7 +23,9 @@ public abstract class Enemigo {
     }
 
     public void recibirDanio(int danio) {
-        this.energia -= danio;
+        if (!this.estaMuerto()) {
+            this.energia -= danio;
+        }
     }
 
     public boolean estaMuerto() {
