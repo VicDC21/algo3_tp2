de04a826029377a82e1f346eba19438aa7cb1af8
Juan S. Burgos <jsburgos@fi.uba.ar>
2023-06-15 11:39:50 -0300

    Uso de mock en tests Arania y Enemigo

--- src/main/test/edu/fiuba/algo3/pruebas/AraniaTest.java
+++ src/main/test/edu/fiuba/algo3/pruebas/AraniaTest.java
@@ -7,16 +7,19 @@ import edu.fiuba.algo3.modelo.Mapa;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mock;
 
 public class AraniaTest {
     @Test
     public void matarUnaAraniaOtorgaCreditosAlAzarEntre0Y10() {
-        Mapa mapa = new Mapa();
-        Constructor constructor = new Constructor(mapa);
+
+        Constructor constructor = mock(Constructor.class);
         Jugador jugador = new Jugador("Prueba", 10, 100, constructor);
         Arania arania = new Arania(1, 1, 1, "Vivo", null);
+
         arania.recibirDanio(1);
         jugador.recibirCreditos(arania.otorgarCredito());
+
         assertTrue(jugador.mostrarCreditos() >= 100 && jugador.mostrarCreditos() <= 110);
     }
 }
