79222b3639235258572020bf4acc8c75fd86866a
Nicomp04 <nicolasmp99@hotmail.com>
2023-06-16 01:23:52 -0300

    primera version de mapa interactivo con opciones no implementadas

--- src/main/java/edu/fiuba/algo3/App.java
+++ src/main/java/edu/fiuba/algo3/App.java
@@ -1,6 +1,7 @@
 package edu.fiuba.algo3;
 
 import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.Shape;
 import javafx.stage.StageStyle;
@@ -13,10 +14,14 @@ import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import java.util.List;
 
@@ -29,8 +34,8 @@ public class App extends Application {
     @Override
     public void start(Stage stage) {
 
-        VBox layout = new VBox();
-        layout.setAlignment(Pos.CENTER);
+        VBox layoutInicial = new VBox();
+        layoutInicial.setAlignment(Pos.CENTER);
 
         TextField textField = new TextField("Ingrese su nombre");
         textField.setAlignment(Pos.CENTER);
@@ -53,19 +58,31 @@ public class App extends Application {
             } catch (InvalidMap ex) {
                 throw new RuntimeException(ex);
             }
+            
             FlowPane mapGrid = new FlowPane();
 
             List<Shape> tiles = juego.getTiles(); // map.getTiles(), for each parcela -> parcela.draw(); que devuelve un Rectangle del color apropiado.
 
             for (Shape tile : tiles) {
-                ((Rectangle) tile).setWidth(tileWidth);
-                ((Rectangle) tile).setHeight(tileHeight);
-                mapGrid.getChildren().add(tile);
+                Rectangle rectangle = ((Rectangle) tile);
+                rectangle.setWidth(tileWidth);
+                rectangle.setHeight(tileHeight);
+                rectangle.setOnMouseClicked(event -> handleTileClick(event));
+
+        //        if (rectangle = Tierra ) { 
+                    ContextMenu contextMenu = createContextMenu(rectangle);
+                    rectangle.setOnContextMenuRequested(event -> contextMenu.show(rectangle, event.getScreenX(), event.getScreenY()));
+          //      }
+
+                mapGrid.getChildren().add(rectangle);
             }
 
+           // HBox layoutJuego = new HBox(mapGrid);
+            
             Scene mapScene = new Scene(mapGrid, tilesPerRow * tileWidth, tilesPerColumn * tileHeight);
             stage.setScene(mapScene);
             System.out.println(mapGrid.getChildren());
+
 //            mapScene.widthProperty().addListener(new ChangeListener<Number>() {
 //                @Override
 //                public void changed(ObservableValue<? extends Number> observableValue, Number oldSceneWidth, Number newSceneWidth) {
@@ -84,16 +101,36 @@ public class App extends Application {
         Image image = new Image("algoDefense.png");
         ImageView imageView = new ImageView(image);
 
-        layout.getChildren().add(imageView);
-        layout.getChildren().add(textField);
-        layout.getChildren().add(button);
-        layout.setSpacing(20);
+        layoutInicial.getChildren().add(imageView);
+        layoutInicial.getChildren().add(textField);
+        layoutInicial.getChildren().add(button);
+        layoutInicial.setSpacing(20);
 
-        var scene = new Scene(layout, 640, 480);
+        var sceneInicial = new Scene(layoutInicial, 640, 480);
         stage.setResizable(false);
         stage.initStyle(StageStyle.UNDECORATED);
-        stage.setScene(scene);
+        stage.setScene(sceneInicial);
         stage.show();
+    }
+
+    private ContextMenu createContextMenu(Rectangle rectangle) {
+        MenuItem opcion1 = new MenuItem("Construir Torre");
+        opcion1.setOnAction(event -> {
+            System.out.println("Se coloco Torre "); //falta implementacion real
+        });
+
+        MenuItem opcion2 = new MenuItem("Cancelar");
+        opcion2.setOnAction(event -> {
+            System.out.println("Cancela Procedimiento");
+        });
+
+        ContextMenu contextMenu = new ContextMenu(opcion1, opcion2);
+
+        return contextMenu;
+    }
+
+    private void handleTileClick(MouseEvent event) {
+      
     }
 
     public static void main(String[] args) {
